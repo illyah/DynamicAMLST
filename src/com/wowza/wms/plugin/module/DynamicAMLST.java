03ad1e0eb7e4ba38b698c1819f1c3c3ef9b6d6a9
Ilja <ilja@stud.fh-dortmund.de>
2018-08-06 21:42:56 +0200

    changed method to get codec info

--- src/com/wowza/wms/plugin/module/DynamicAMLST.java
+++ src/com/wowza/wms/plugin/module/DynamicAMLST.java
@@ -7,45 +7,46 @@ import com.wowza.wms.stream.*;
 import com.wowza.wms.amf.AMFDataList;
 import com.wowza.wms.application.*;
 import com.wowza.wms.client.IClient;
+import com.wowza.wms.httpstreamer.cupertinostreaming.file.HTTPStreamerCupertinoIndexFile;
+import com.wowza.wms.httpstreamer.cupertinostreaming.file.IHTTPStreamerCupertinoIndex;
+import com.wowza.wms.httpstreamer.cupertinostreaming.file.IMediaReaderCupertino;
 import com.wowza.wms.httpstreamer.model.IHTTPStreamerSession;
 import java.util.List;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class DynamicAMLST extends ModuleBase {
+	
 	private IApplicationInstance appIns = null;
+	private IMediaReaderCupertino mediaReader = null;
+	
 	class MyMediaListProvider implements IMediaListProvider
 	{
 		public MediaList resolveMediaList(IMediaListReader mediaListReader, IMediaStream stream, String streamName)
 		{
-			List<String> resolutions = new ArrayList<String>();
-			
-			resolutions.add(".mp4");
-			resolutions.add("_720p.mp4");
-			resolutions.add("_1080p.mp4");
-			
-			streamName = streamName.substring(0, streamName.lastIndexOf('.'));
+			List<String> renditions = getRenditions(streamName);
 			MediaList mediaList = new MediaList();
 			MediaListSegment segment = new MediaListSegment();
 			mediaList.addSegment(segment);
 			
-			Iterator<String> iter = resolutions.iterator(); 
+			Iterator<String> iter = renditions.iterator(); 
 			while(iter.hasNext())
 			{
 				String res = iter.next(); 
-				ICodecInfoRetrieve info = getInfo(streamName + res, stream);
+				IHTTPStreamerCupertinoIndex info = readMedia(stream, res);
 				if(info != null)
 				{
 					MediaListRendition rendition = new MediaListRendition();
 					segment.addRendition(rendition);
-					rendition.setName("mp4:" + streamName + res);
+					rendition.setName("mp4:" + res);
 					rendition.setBitrateAudio(info.getAudioBitrate());
-					rendition.setBitrateVideo(info.getVideoBitrate());
+					rendition.setBitrateVideo(info.getVideoBitrateAverage());
 					
-					rendition.setWidth(info.getVideoCodec().getFrameHeight());
-					rendition.setHeight(info.getVideoCodec().getFrameWidth());
-					rendition.setAudioCodecId(info.getAudioCodec().toCodecsStr());
-					rendition.setVideoCodecId(info.getVideoCodec().toCodecsStr());
+					rendition.setWidth(info.getCodecInfoVideo().getFrameWidth());
+					rendition.setHeight(info.getCodecInfoVideo().getFrameHeight());
+					rendition.setAudioCodecId(info.getCodecInfoAudio().toCodecsStr());
+					rendition.setVideoCodecId(info.getCodecInfoVideo().toCodecsStr());
 				}
 			}
 
@@ -92,17 +93,54 @@ public class DynamicAMLST extends ModuleBase {
 	public void onHTTPSessionCreate(IHTTPStreamerSession httpSession) {
 		getLogger().info("onHTTPSessionCreate: " + httpSession.getSessionId());
 	}
-
-	public ICodecInfoRetrieve getInfo(String streamName, IMediaStream stream)
+	
+	public List<String> getRenditions(String streamName)
 	{
-		getLogger().debug("stream: " + streamName);
+		String baseName = streamName.substring(0, streamName.lastIndexOf('.'));
+		List<String> files = new ArrayList<String>();
 		
-		ICodecInfoRetrieve thisRetrieve = new FileCodecRetrieve(this.appIns, stream, streamName);
-		thisRetrieve.open();
-		if (thisRetrieve.getCompleted() == true)
+		File streamFile = new File(this.appIns.getStreamStorageDir() + "/" + streamName);
+		
+		if(streamFile.exists() && streamFile.isFile())
 		{
-			return thisRetrieve;
+			File storageDir = new File(this.appIns.getStreamStorageDir());
+		
+			for(File file: storageDir.listFiles())
+			{
+				if(file.getName().startsWith(baseName))
+				{
+					files.add(file.getName());
+				}
+			}
 		}
-		return null;
+		return files;	
+	}
+	
+	
+	public IHTTPStreamerCupertinoIndex readMedia(IMediaStream stream, String streamName)
+	{
+		try
+		{
+			IMediaReader reader = MediaReaderFactory.getInstance(this.appIns, this.appIns.getVHost().getMediaReaders(), "mp4cupertino");
+			if (reader != null)
+			{
+		    	String basePath = appIns.getStreamStoragePath();
+		    	reader.init(this.appIns, stream, "mp4cupertino", basePath, streamName);
+		    	reader.open(basePath, streamName);
+		    	mediaReader = (IMediaReaderCupertino) reader;
+			}
+		}
+		catch (Exception e)
+		{
+			getLogger().info("Broke it " + e.toString());
+		}
+					
+		IHTTPStreamerCupertinoIndex indexer = null;
+		indexer = new HTTPStreamerCupertinoIndexFile();
+					
+		mediaReader.indexFile(indexer);			
+		mediaReader.close();
+
+		return indexer;
 	}
 }
